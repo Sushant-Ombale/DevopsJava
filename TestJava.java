9ba780fcbfcea9330aa4a54ff4a94f35bd81ae2b
Sushant-Ombale <37531432+Sushant-Ombale@users.noreply.github.com>
2023-03-14 14:56:56 +0900

    Update TestJava.java

--- TestJava.java
+++ TestJava.java
@@ -3,7 +3,7 @@ public class TestJava {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("Nice to meet you");
-
+		System.out.println("See you soon");
 	}
 
 }
